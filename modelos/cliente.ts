0843563f276986dc7a7b1859fca336a802203353
Nicolas Lima <nichollaslimma734@gmail.com>
2022-10-10 20:34:06 -0300

    atualizado cadastro de dependente

--- modelos/cliente.ts
+++ modelos/cliente.ts
@@ -12,5 +12,4 @@ export default class Cliente {
     public documentos: Documento[] = []
     public dependentes: Cliente[] = []
     public titular: Cliente
-
 }
--- negocio/cadastroCliente.ts
+++ negocio/cadastroCliente.ts
@@ -25,22 +25,24 @@ export default class CadastroCliente extends Casdastro{
         this.cadastrarTelefone(cliente,telefone)
         this.cadastrarEndereco(cliente,endereco)
         this.cadastroDocumento(cliente)
-        const dependente = this.cadastrartDependente(cliente)
-        cliente.dependentes.push(dependente)
+        const dependente = this.cadastrarDependente(cliente)
+        cliente.dependentes.push(...dependente)
 
         // let listagemCliente = new ListagemCliente(cliente)
         // listagemCliente.listar()
 
-        console.log('CLIENTE-----------------',cliente);
-
-        console.log('DEPENDENTE-----------------',dependente);
-
-        // console.log(dependente.telefones);
+        console.log(cliente);
+        console.log(dependente);
 
         console.log(`\n Cadastro concluído. \n`);
 
     }
 
+
+    /**
+     * 
+     * Cadastro Dos Clientes
+     */
     cadastrarDadosCliente(cliente:Cliente){
 
         console.log(`\nInício do cadastro do cliente.\n`);
@@ -64,15 +66,19 @@ export default class CadastroCliente extends Casdastro{
     }
 
 
+    /**
+     * 
+     * Cadastro Dos Telefones
+     */
     cadastrarTelefone(cliente:Cliente, telefone:Telefone){
         console.log(`\nInício do cadastro do telefone.\n`);
 
-        let execucao = true
+        let execucao:Boolean = true
 
         while(execucao){
             let cadastrar = this.entrada.receberTexto(`Deseja cadastrar um telefone ? [ SIM / NAO ] `)
             if(cadastrar.toLocaleUpperCase() === "SIM"){
-                let dddNumero = this.entrada.receberTexto(`Por favor informe o DDD e o numero de telefone, no padrão (xx) xxxx-xxxx `)
+                let dddNumero = this.entrada.receberTexto(`Por favor informe o DDD e o numero de telefone, no padrão (xx) xxxx-xxxx: `)
 
                 let separacao = dddNumero.split(')')
                 let dadosNumero = separacao[0] + ")-" + separacao[1]
@@ -86,6 +92,8 @@ export default class CadastroCliente extends Casdastro{
                 telefone.numero = numero
 
                 cliente.telefones.push(telefone)
+                console.log('\n');
+
             }else{
                 execucao = false
             }
@@ -94,11 +102,13 @@ export default class CadastroCliente extends Casdastro{
     }
 
 
+    /**
+     * 
+     * Cadastro Do Endereco
+     */
     cadastrarEndereco(cliente:Cliente, endereco:Endereco){
 
         console.log(`\nInício do cadastro do endereço.\n`);
-
-        // ---- endereco do cliente ----
         let rua = this.entrada.receberTexto(`Por favor informe a rua: `)
         let bairro = this.entrada.receberTexto(`Por favor informe o bairro: `)
         let cidade = this.entrada.receberTexto(`Por favor informe a cidade: `)
@@ -112,28 +122,27 @@ export default class CadastroCliente extends Casdastro{
         endereco.estado = estado
         endereco.pais = pais
         endereco.codigoPostal = cep
-
         cliente.endereco = endereco
     }
 
-    cadastroDocumento(cliente:Cliente){
 
-        let execucao = true 
+    /**
+     * 
+     * Cadastro Dos Documentos
+     */
+    cadastroDocumento(cliente:Cliente){
+        let execucao:Boolean = true
 
         while(execucao){
+            console.log('\n');
             let cadastrar = this.entrada.receberTexto(`Deseja cadastrar um documento ? [ SIM / NAO ] `)
             if(cadastrar.toLocaleUpperCase() === "SIM"){
                 let documento = new Documento()
-                
                 console.log(`\nOpções de documentos:
-
             [1] CPF
-
             [2] RG
-
             [3] Passaporte
                 `);
-                
                 let opcaoTipoDocumento = this.entrada.receberNumero('Escolha um tipo de documento em que deseja cadastrar: ')
                 let tipoDocumento
 
@@ -153,50 +162,54 @@ export default class CadastroCliente extends Casdastro{
 
                 let numero = this.entrada.receberTexto(`Digite o numero do ${tipoDocumento} por favor: `)
                 let dataExpedicao = this.entrada.receberData(`Digite a data de expedição do ${tipoDocumento}`)
-
                 documento.tipo = tipoDocumento
                 documento.numero = numero
                 documento.dataExpedicao = dataExpedicao
-                
                 cliente.documentos.push(documento)
             }else{
                 execucao = false
             }
         }
-
     }
 
-    //EM PROGRESSO
-    cadastrartDependente(titular:Cliente){
-        let convidado = this.entrada.receberTexto(`Deseja cadastrar um convidado para este titular ? [ SIM / NAO ]`)
-        if(convidado.toLocaleUpperCase() === 'SIM'){
 
-            let dependente = new Cliente()
+    /**
+     * 
+     * Cadastro Dos Dependentes
+     */
+    cadastrarDependente(titular:Cliente){
+        let execucao:Boolean = true
+        let dependentes:Array<Cliente> = []
 
-            let nome = this.entrada.receberTexto(`Por favor informe o nome do convidado: `)
-            let nomeSocial = this.entrada.receberTexto(`Por favor informe o nome social do convidado: `)
-            let dataNascimento = this.entrada.receberData(`Por favor informe a data de nascimento do convidado`)
-            let dataCadastro = new Date().toLocaleString().split(' ')
+        while(execucao){
 
-            let cadData = dataCadastro[0].split('/')
-            let ano = new Number(cadData[2].valueOf()).valueOf()
-            let mes = new Number(cadData[1].valueOf()).valueOf()
-            let dia = new Number(cadData[0].valueOf()).valueOf()
-            let dataCadastroFormatado =  new Date(ano, mes, dia);
+            console.log('\n');
+            let convidado = this.entrada.receberTexto(`Deseja cadastrar um convidado para este titular ? [ SIM / NAO ] `)
 
-            dependente.nome = nome
-            dependente.nomeSocial = nomeSocial
-            dependente.dataNascimento = dataNascimento
-            dependente.dataCadastro =dataCadastroFormatado
+            if(convidado.toLocaleUpperCase() === 'SIM'){
+                let dependente = new Cliente()
+                let nome = this.entrada.receberTexto(`Por favor informe o nome do convidado: `)
+                let nomeSocial = this.entrada.receberTexto(`Por favor informe o nome social do convidado: `)
+                let dataNascimento = this.entrada.receberData(`Por favor informe a data de nascimento do convidado`)
+                let dataCadastro = new Date().toLocaleString().split(' ')
+                let cadData = dataCadastro[0].split('/')
+                let ano = new Number(cadData[2].valueOf()).valueOf()
+                let mes = new Number(cadData[1].valueOf()).valueOf()
+                let dia = new Number(cadData[0].valueOf()).valueOf()
+                let dataCadastroFormatado =  new Date(ano, mes, dia);
+                dependente.nome = nome
+                dependente.nomeSocial = nomeSocial
+                dependente.dataNascimento = dataNascimento
+                dependente.dataCadastro =dataCadastroFormatado
+                dependente.endereco = (titular.endereco.clonar() as Endereco)
+                dependente.telefones = (titular.telefones.map(telefone => telefone.clonar()) as Telefone[])
+                dependente.titular = titular
+                dependentes.push(dependente)
 
-            dependente.endereco = (titular.endereco.clonar() as Endereco)
-            dependente.telefones.push((titular.telefones[0].clonar() as Telefone))
-
-            dependente.titular = titular
-
-            return dependente
+            }else{
+                execucao = false
+                return dependentes
+            }
         }
     }
-
-
 }
